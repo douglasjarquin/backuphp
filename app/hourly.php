1d04a725001c509cee66550f24341d248bd01852
Douglas Jarquin <douglasjarquin@gmail.com>
2009-06-09 15:15:16 -0400

    New variable names.

--- app/hourly.php
+++ app/hourly.php
@@ -59,14 +59,14 @@ function backupDBs($hostname, $username, $password, $prefix = '') {
 
   // Run backups on each database in the array
   foreach ($databases as $database) {
-    $database_structure = $database . '_structure';
-    $database_content = $database . '_content';
-    `mysqldump $MYSQL_OPTIONS --no-data --host=$hostname --user=$username --password='$password' $database | bzip2  > $database_structure.sql.bz2`;
-    `mysqldump $MYSQL_OPTIONS --host=$hostname --user=$username --password='$password' $database | bzip2 > $database_content.sql.bz2`;
-    $s3->putObjectFile('$database_structure.sql.bz2', $BACKUP_BUCKET, s3Path($prefix,'/'.$database_structure.sql.bz2));
-    $s3->putObjectFile('$database_content.sql.bz2', $BACKUP_BUCKET, s3Path($prefix,'/'.$database_content.sql.bz2));
+     $structure = $database .'_structure';
+     $content = $database .'_content';
+    `mysqldump $MYSQL_OPTIONS --no-data --host=$hostname --user=$username --password='$password' $database | bzip2  > $structure.sql.bz2`;
+    `mysqldump $MYSQL_OPTIONS --host=$hostname --user=$username --password='$password' $database | bzip2 > $content.sql.bz2`;
+    $s3->putObjectFile("$structure.sql.bz2", $BACKUP_BUCKET, s3Path($prefix, "/".$database."_structure.sql.bz2"));
+    $s3->putObjectFile("$content.sql.bz2", $BACKUP_BUCKET, s3Path($prefix, "/".$database."_content.sql.bz2"));
 
-    `rm -rf $database_structure.sql.bz2 $database_content.sql.bz2`;
+    `rm -rf $structure.sql.bz2 $content.sql.bz2`;
   }
 
 }
